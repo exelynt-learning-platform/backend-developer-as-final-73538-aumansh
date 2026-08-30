e7997b0f11f7ba644b888ef92a8d89b4f19f9724
unknown <123456+AUMANSH@users.noreply.github.com>
2026-08-30 21:48:47 +0530

    Remove python helper scripts

--- fix_jwt_parse.py
+++
@@ -1,9 +0,0 @@
-import os
-filepath = "src/main/java/com/example/bookingsystem/security/JwtTokenProvider.java"
-with open(filepath, "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = content.replace(".parse(token);", ".parseClaimsJws(token);")
-
-with open(filepath, "w", encoding="utf-8") as f:
-    f.write(content)
--- fix_rc2.py
+++
@@ -1,9 +0,0 @@
-import os
-filepath = "src/main/java/com/example/bookingsystem/controller/ResourceController.java"
-with open(filepath, "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = content.replace("import java.util.List;\n", "")
-
-with open(filepath, "w", encoding="utf-8") as f:
-    f.write(content)
